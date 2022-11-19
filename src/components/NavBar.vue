2fdbb395a11a50b141ea06973dbafb7e0324068e
Simone Kruegel <simone.kruegel04@gmail.com>
2022-11-19 00:13:54 +0000

    deleted duplicate home

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,27 @@
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/users">Users</router-link>
-          <router-link class="nav-link" to="/courses">Courses</router-link>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/users">Users</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/courses">Courses</router-link>
           </li>
         </ul>
       </div>
